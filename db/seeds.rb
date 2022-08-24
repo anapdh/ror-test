3b9876a877c487251bc544fabb10d807601f7450
Ana Paula <ana_d_hubner@hotmail.com>
2022-08-24 18:27:48 -0300

    Add seeds for contacts and properties

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@ users = [
 users.each do |user|
   User.create(user)
 end
+
+contacts = [
+  { name: 'Provider1', contact_type: 'Provider', email: 'abc@example.com' },
+  { name: 'Client1', contact_type: 'Client', email: 'dfg@example.com' }
+]
+
+contacts.each do |contact|
+  Contact.create(contact)
+end
+
+properties = [
+  { name: 'Property1', plantation_area: 100, plant_specie: 'Cucumber', plants_row_spacing: 2, plants_column_spacing: 2, contact_id: 1 },
+  { name: 'Property2', plantation_area: 200, plant_specie: 'Tomato', plants_row_spacing: 3, plants_column_spacing: 3, contact_id: 1 },
+  { name: 'Property3', plantation_area: 300, plant_specie: 'Potato', plants_row_spacing: 4, plants_column_spacing: 4, contact_id: 2 }
+]
+
+properties.each do |property|
+  Property.create(property)
+end
