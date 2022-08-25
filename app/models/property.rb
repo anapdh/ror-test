c150f1715c4b1dd72efa56474ac91e2168909e36
Ana Paula <ana_d_hubner@hotmail.com>
2022-08-25 17:00:59 -0300

    Add limit of properties per Provider

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,6 @@
 class Property < ApplicationRecord
   before_save :plants_number_calc
+  before_create :set_properties_max
 
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates :plantation_area, presence: true, numericality: { only_integer: true, greater_than: 0 }
@@ -12,5 +13,12 @@ class Property < ApplicationRecord
   def plants_number_calc
     calc = (plantation_area / (plants_row_spacing * plants_column_spacing)).to_i
     self.plants_number = calc
+  end
+
+  def set_properties_max
+    if self.contact.properties.count >= 3
+      errors.add(:base, "Limit of properties is 3")
+      throw(:abort)
+    end
   end
 end
