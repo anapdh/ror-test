97ede737ecb8c93bd94b112b989f26260dc23d64
Ana Paula <ana_d_hubner@hotmail.com>
2022-08-25 17:01:08 -0300

    Fix some bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :contacts do
-    resources :properties, module: :contacts
-  end
-
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "contacts#index"
+
+  resources :contacts do
+    resources :properties, module: :contacts
+  end
 end
