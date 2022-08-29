9a22947436665fac7ba98cf6b50cc1d422e10ec2
Ana Paula <ana_d_hubner@hotmail.com>
2022-08-29 16:08:12 -0300

    Add dropdown select to plant specie

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -6,6 +6,29 @@ class Plant < ApplicationRecord
 
   belongs_to :property
 
+  PLANT_SPECIES_OPTIONS = [
+    'Apple',
+    'Beans',
+    'Broccoli',
+    'Cauliflower',
+    'Cabbage',
+    'Carrot',
+    'Corn',
+    'Grape',
+    'Lettuce',
+    'Melon',
+    'Onion',
+    'Peas',
+    'Potato',
+    'Pumpkin',
+    'Rice',
+    'Spinach',
+    'Squash',
+    'Tomato',
+    'Watermelon',
+    'Other'
+  ]
+
   def set_plant_specie_name
     property.plant_specie = name
     property.save
